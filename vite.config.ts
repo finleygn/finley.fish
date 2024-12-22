2e128483bb9d5f0a184f9f12ad276e21dd29082b
finleygn <finleygarton12@gmail.com>
2024-12-22 01:56:12 +0000

    compress more

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import glsl from 'vite-plugin-glsl';
+import { viteSingleFile } from "vite-plugin-singlefile"
+import { createHtmlPlugin } from 'vite-plugin-html'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [glsl({ compress: true })]
+  plugins: [glsl({ compress: true }), viteSingleFile(), createHtmlPlugin({ minify: true })],
+  build: {
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000,
+  }
 });
